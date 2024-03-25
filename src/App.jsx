2293e4350829cd0456891d6a2e1bd32ca6ef44f1
Aftab1112 <you@example.com>
2024-03-25 17:42:32 +0530

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,48 @@
 // App.js
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import ERC20Token from './components/ERC20Token';
-import { useState } from 'react';
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import ERC20Token from "./components/ERC20Token";
+import { useState } from "react";
 
 const App = () => {
   const [show, setShow] = useState(false);
 
   return (
-    <div className='w-full h-screen'>
-      <div className='h-[65px] bg-slate-700 flex items-center  '>
-        <div className='w-full px-5 flex justify-between items-center '>
+    <div className="w-full h-screen">
+      <div className="h-[65px] bg-slate-700 flex items-center  ">
+        <div className="w-full px-5 flex justify-between items-center ">
           <h1>Testing</h1>
           <ConnectButton />
         </div>
       </div>
 
-       {/* Main Content */}
-       <div className='flex-grow flex'>
+      {/* Main Content */}
+      <div className="flex-grow flex">
         {/* Sidebar */}
-        <div className='w-[30%] px-6 bg-black'>
-          <div className='my-10 flex flex-col gap-14 text-white'>
-            <button onClick={() => setShow(!show)} className='text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300'>Create Your Own ERC20 Token</button>
-            <button className='text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300'>Create Your Own NFT</button>
-            <button className='text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300'>Create Your Own Dapp</button>
-            <button className='text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300'>Create Your Own Defi Platform</button>
+        <div className="w-[30%] px-6 bg-gradient-to-b from-slate-800 to bg-gray-500 h-screen flex flex-col items-center justify-center">
+          <h1 className="text-5xl text-white font-semibold mb-10">
+            Get Started <br /> <span className="ml-16 ">With...</span>
+          </h1>
+          <div className="my-10 flex flex-col gap-14 text-white">
+            <button
+              onClick={() => setShow(!show)}
+              className="text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300"
+            >
+              Create Your Own ERC20 Token
+            </button>
+            <button className="text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300">
+              Create Your Own NFT
+            </button>
+            <button className="text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300">
+              Create Your Own Dapp
+            </button>
+            <button className="text-xl py-3 px-4 rounded-md bg-gradient-to-r from-cyan-600 to-blue-800 hover:from-cyan-700 hover:to-blue-900 focus:outline-none focus:ring-2 focus:ring-cyan-500 transition duration-300">
+              Create Your Own Defi Platform
+            </button>
           </div>
         </div>
 
         {/* Main Content Area */}
-        <div className='w-[80%] bg-gray-100 p-8'>
-          {show && <ERC20Token />}
-        </div>
+        <div className="w-[80%] ">{show && <ERC20Token />}</div>
       </div>
     </div>
   );
--- src/components/ERC20Token.jsx
+++ src/components/ERC20Token.jsx
@@ -1,7 +1,12 @@
-import React, { useState } from 'react';
-import { ethers } from 'ethers';
-import { useAccount, useTransactionReceipt, useWaitForTransactionReceipt, useWriteContract } from 'wagmi';
-import abi from '../Contracts/abi/factory.json';
+import React, { useState } from "react";
+import { ethers } from "ethers";
+import {
+  useAccount,
+  useTransactionReceipt,
+  useWaitForTransactionReceipt,
+  useWriteContract,
+} from "wagmi";
+import abi from "../Contracts/abi/factory.json";
 // import ercabi from '../Contracts/abi/ERC20.json';
 
 const ERC20Token = () => {
@@ -12,8 +17,6 @@ const ERC20Token = () => {
   const [tokenName, setTokenName] = useState("");
   const [symbol, setSymbol] = useState("");
   const [supply, setSupply] = useState("");
-
-
 
   const handleContractNameChange = (e) => {
     setContractName(e.target.value);
@@ -41,67 +44,116 @@ const ERC20Token = () => {
     // } catch (error) {
     //   console.error('An error occurred', error);
     // }
-  }
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       writeContract({
         abi,
-        address: '0x360E40E234B94a6E549ae9080E0CBe03Aaa43CA9',
-        functionName: 'createToken',
-        args: [
-          contractName,
-          tokenName,
-          18,
-          address,
-        ]
+        address: "0x360E40E234B94a6E549ae9080E0CBe03Aaa43CA9",
+        functionName: "createToken",
+        args: [contractName, tokenName, 18, address],
       });
     } catch (error) {
-      console.error('Error executing contract:', error);
+      console.error("Error executing contract:", error);
     }
   };
 
-  const { isLoading: isConfirming, isSuccess: isConfirmed } = useWaitForTransactionReceipt({
-    hash: hash,
-  })
+  const { isLoading: isConfirming, isSuccess: isConfirmed } =
+    useWaitForTransactionReceipt({
+      hash: hash,
+    });
 
   const token = useTransactionReceipt({
-    hash
-  })
+    hash,
+  });
 
-  let dt = token.data.logs[0].data;
-  let tokenAddress = ethers.utils.defaultAbiCoder.decode(["address"], dt).toString();
+  // let dt = token.data.logs[0].data;
+  // let tokenAddress = ethers.utils.defaultAbiCoder.decode(["address"], dt).toString();
 
-  console.log(token);
+  // console.log(token);
 
   return (
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-full bg-gradient-to-b from-indigo-300 to-gray-900 ">
       {!isConfirmed && (
-        <form className='flex flex-col gap-6 bg-gray-100 p-8 rounded-lg shadow-md' onSubmit={handleSubmit}>
-          <input required className='bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white' type="text" placeholder='Contract Name' onChange={handleContractNameChange} value={contractName} />
-          <input required className='bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white' type="text" placeholder='Token Name' onChange={handleTokenNameChange} value={tokenName} />
-          <input required className='bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white' type="text" placeholder='Symbol' onChange={handleSymbolChange} value={symbol} />
-          <button disabled={isPending} className='bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300' type="submit">{isPending ? 'Confirming' : 'Submit'}</button>
+        <form
+          className="flex flex-col gap-6  bg-gray-100 p-8 rounded-lg shadow-md w-[490px]  h-[350px]  border border-blue-100  "
+          onSubmit={handleSubmit}
+        >
+          <input
+            required
+            className="bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white mt-3"
+            type="text"
+            placeholder="Contract Name"
+            onChange={handleContractNameChange}
+            value={contractName}
+          />
+          <input
+            required
+            className="bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white"
+            type="text"
+            placeholder="Token Name"
+            onChange={handleTokenNameChange}
+            value={tokenName}
+          />
+          <input
+            required
+            className="bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white"
+            type="text"
+            placeholder="Symbol"
+            onChange={handleSymbolChange}
+            value={symbol}
+          />
+          <button
+            disabled={isPending}
+            className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300 mt-5"
+            type="submit"
+          >
+            {isPending ? "Confirming" : "Submit"}
+          </button>
           {isConfirming && <div>Waiting for confirmation...</div>}
           {isConfirmed && <div>Transaction confirmed.</div>}
         </form>
       )}
 
       {isConfirmed && (
-        <div className='flex flex-col gap-6 bg-gray-100 p-8 rounded-lg shadow-md'>
+        <div className="flex flex-col gap-6 bg-gray-100 p-8 rounded-lg shadow-md">
           {isConfirmed && (
             <div className="bg-gray-200 rounded-md px-4 py-2">
-              <span className="text-gray-700 font-bold">Transaction Confirmed</span> <br />
+              <span className="text-gray-700 font-bold">
+                Transaction Confirmed
+              </span>{" "}
+              <br />
               <span className="text-gray-700">Previous Data:</span> <br />
-              <span className="text-gray-700">Contract Name: {contractName}</span> <br />
-              <span className="text-gray-700">Token Name: {tokenName}</span> <br />
+              <span className="text-gray-700">
+                Contract Name: {contractName}
+              </span>{" "}
+              <br />
+              <span className="text-gray-700">
+                Token Name: {tokenName}
+              </span>{" "}
+              <br />
               <span className="text-gray-700">Symbol: {symbol}</span>
             </div>
           )}
-          <form className='flex flex-col gap-6 mt-6' onSubmit={handleSubmit_next}>
-            <input className='bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white' type="number" placeholder='Supply' onChange={handleSupplyChange} value={supply} />
-            <button className='bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300' type="submit">Submit</button>
+          <form
+            className="flex flex-col gap-6 mt-6"
+            onSubmit={handleSubmit_next}
+          >
+            <input
+              className="bg-gray-200 rounded-md px-4 py-2 outline-none focus:bg-white"
+              type="number"
+              placeholder="Supply"
+              onChange={handleSupplyChange}
+              value={supply}
+            />
+            <button
+              className="bg-blue-500 text-white font-bold py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300"
+              type="submit"
+            >
+              Submit
+            </button>
           </form>
         </div>
       )}
